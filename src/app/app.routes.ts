c04ae6c6a94be8fbd6916d4e1b800aad700075de
Alejandro Gonzalez <alex1123.nel@gmail.com>
2024-06-10 20:22:40 -0600

    Not found component (404) added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { NofoundComponent } from './shared/nofound/nofound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'**',
+    component: NofoundComponent
+  }
+];
